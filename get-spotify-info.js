bc80cac2bab38e7c89093185f8968a1b928a696f
dillonupgradeit <info@upgrade-sf.com>
2020-12-23 11:00:45 -0800

    Add initial spotify router

---
+++ get-spotify-info.js
@@ -0,0 +1,9 @@
+function getSpotifyInfo(){
+    scrapedSong = {};
+    scrapedSong.domain = "open.spotify.com";
+    scrapedSong.origin = "SPOTIFY";
+    scrapedSong.mediaType = "AUDIO"
+
+    return scrapedSong;
+} 
+getSpotifyInfo();
